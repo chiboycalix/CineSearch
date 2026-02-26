08ece4fc8587786dbd4267cee1b7ea21f25a16de
hafsah517 <hafsahishaq9@gmail.com>
2026-02-26 11:31:53 +0100

    Implemented footer section

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-
-import { Outlet, useLocation } from "react-router"
-import Header from "./components/layout/Header"
+import { Outlet, useLocation } from "react-router";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/footer";
 
 function App() {
   const location = useLocation();
   console.log({ location });
   return (
-    <div className="min-h-screen bg-[#221010]">
-      <Header />
-
-      <div className={location?.pathname === "/details" ? "px-0" : "px-20"}>
-        <Outlet />
+    <div>
+      <div className=" bg-[#221010]">
+        <Header />
+        <div className={location?.pathname === "/details" ? "px-0" : "px-20"}>
+          <Outlet />
+        </div>
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
